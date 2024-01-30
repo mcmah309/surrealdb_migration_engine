05c29f7637f8f473480d32b64b668dce36459dba
mcmah309 <dillonmcmahon79@gmail.com>
2024-01-30 07:07:40 +0000

    feat: create_migration_table_and_schema_if_not_exists

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
-use std::error::Error;
+use std::{borrow::Cow, error::Error};
 
 use anyhow::bail;
-use serde::Deserialize;
+use chrono::{DateTime, Utc};
+use regex::Regex;
+use rust_embed::RustEmbed;
+use serde::{Deserialize, Serialize};
 use serde_json::{map::Values, Value};
 use surrealdb::{engine::remote::ws::Client, Surreal};
 
@@ -15,8 +18,9 @@ impl<'a> Migrations<'a> {
     }
 
     pub async fn run(&self) -> anyhow::Result<()> {
-        create_migration_table_if_not_exists(self.client).await?;
-
+        if (create_migration_table_and_schema_if_not_exists(self.client).await?) {
+            //run_new_migrations(self.client).await?;
+        }
         Ok(())
     }
 }
@@ -27,20 +31,38 @@ enum MigrationsError {
     QueryResultNotAnObject(String, String),
     #[error("key '{0}' not found in query result '{1}'")]
     QueryResultKeyNotFound(String, String),
+    #[error("File name '{0}' is malformed. Expected format e.g.: '0001_name.sursql'")]
+    FileNameMalformed(String),
+    #[error("Cannot load file '{0}'")]
+    CannotLoadFile(String),
+    #[error("Error running migration '{0}'")]
+    WrappedError(#[from] Box<dyn Error + Send + Sync>),
 }
 
+#[derive(Debug, Deserialize, Serialize)]
+struct Migration {
+    file_name: String,
+    number: i32,
+    date_ran: Option<DateTime<chrono::Utc>>,
+}
 
-async fn create_migration_table_if_not_exists(
+#[derive(Debug)]
+struct SqlFile {
+    file_name: String,
+    number: i32,
+    sql: String,
+}
+
+/// Creates the migration table and schema if it does not exist.
+/// Returns true if the table was created, false if it already existed.
+async fn create_migration_table_and_schema_if_not_exists(
     client: &Surreal<Client>,
-) -> anyhow::Result<()> {
+) -> anyhow::Result<bool> {
     let get_migration_db = r#"
 INFO FOR DB;
     "#;
 
-    let result: Vec<Value> = client
-        .query(get_migration_db)
-        .await?
-        .take(0)?;
+    let result: Vec<Value> = client.query(get_migration_db).await?.take(0)?;
 
     let Some(db_info) = result.get(0) else {
         bail!(
@@ -53,19 +75,48 @@ INFO FOR DB;
         .as_object()
         .ok_or_else(|| MigrationsError::QueryResultNotAnObject("".into(), db_info.to_string()))?
         .get("tables")
-        .ok_or_else(|| MigrationsError::QueryResultKeyNotFound("tables".into(),db_info.to_string()))?
+        .ok_or_else(|| {
+            MigrationsError::QueryResultKeyNotFound("tables".into(), db_info.to_string())
+        })?
         .as_object()
-        .ok_or_else(|| MigrationsError::QueryResultNotAnObject("tables".into(), db_info.to_string()))?;
+        .ok_or_else(|| {
+            MigrationsError::QueryResultNotAnObject("tables".into(), db_info.to_string())
+        })?;
 
-    let has_migrations_table = tables
-        .get("migrations")
-        .is_some();
+    let has_migrations_table = tables.get("migrations").is_some();
 
     if has_migrations_table {
-        return Ok(());
+        return Ok(false);
     }
 
-    let sql = r#"
+    let schemas = get_sql_files::<SchemaFiles>().await?;
+
+    let create_schema_sql = schemas
+        .iter()
+        .map(|migration| migration.sql.as_str())
+        .collect::<Vec<_>>()
+        .join("\n");
+
+    let migrations = get_sql_files::<MigrationFiles>().await?;
+
+    let insert_existing_migrations_sql: String = migrations
+        .into_iter()
+        .map(|migration| Migration {
+            file_name: migration.file_name,
+            number: migration.number,
+            date_ran: None,
+        })
+        .map(|migration| {
+            Ok(format!(
+                "INSERT INTO migrations {};",
+                serde_json::to_string(&migration)?
+            ))
+        })
+        .collect::<anyhow::Result<Vec<_>>>()?
+        .join("\n");
+
+    let sql = format!(
+        r#"
 BEGIN TRANSACTION;
 
 DEFINE TABLE migrations SCHEMAFULL;
@@ -74,13 +125,73 @@ DEFINE FIELD fileName ON TABLE migrations TYPE string;
 DEFINE FIELD number ON TABLE migrations TYPE int;
 DEFINE FIELD dateRan ON TABLE migrations TYPE datetime;
 
+{}
+
+{}
+
 COMMIT TRANSACTION;
+    "#,
+        create_schema_sql, insert_existing_migrations_sql
+    );
+
+    let _ = client.query(sql).await?.check()?;
+
+    Ok(true)
+}
+
+#[derive(RustEmbed)]
+#[folder = "migrations"]
+struct MigrationFiles;
+
+#[derive(RustEmbed)]
+#[folder = "schema"]
+struct SchemaFiles;
+
+async fn get_migrations_in_db(client: &Surreal<Client>) -> anyhow::Result<Vec<Migration>> {
+    let sql = r#"
+SELECT * FROM migrations;
     "#;
 
-    let _ = client
-        .query(sql)
-        .await?
-        .check()?;
+    let migrations: Vec<Migration> = client.query(sql).await?.take(0)?;
 
-    Ok(())
+    Ok(migrations)
+}
+
+async fn get_sql_files<F: rust_embed::RustEmbed>() -> anyhow::Result<Vec<SqlFile>> {
+    let number_re = Regex::new(r"^\d+").unwrap();
+
+    let number_and_file_name: Vec<(i32, Cow<str>)> = F::iter()
+        .map(|file_name| {
+            let migration_file_name = file_name.to_string();
+            let migration_number = (|| {
+                number_re
+                    .captures(&file_name)?
+                    .get(0)?
+                    .as_str()
+                    .parse::<i32>()
+                    .ok()
+            })()
+            .ok_or_else(|| MigrationsError::FileNameMalformed(migration_file_name.clone()))?;
+            Ok::<_, MigrationsError>((migration_number, file_name))
+        })
+        .collect::<Result<Vec<_>, MigrationsError>>()?;
+
+    let schemas: Vec<SqlFile> = number_and_file_name
+        .into_iter()
+        .map(|(number, file_name)| {
+            Ok(SqlFile {
+                file_name: file_name.to_string(),
+                number: number,
+                sql: String::from_utf8_lossy(
+                    F::get(file_name.as_ref())
+                        .ok_or_else(|| MigrationsError::CannotLoadFile(file_name.into_owned()))?
+                        .data
+                        .as_ref(),
+                )
+                .to_string(),
+            })
+        })
+        .collect::<Result<Vec<_>, MigrationsError>>()?;
+
+    Ok(schemas)
 }
