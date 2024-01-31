dbbfad46958455bbad762156e5d5cf3f8337864b
mcmah309 <dillonmcmahon79@gmail.com>
2024-01-31 08:20:41 +0000

    chore: Update docs and dependencies

--- tests/mod.rs
+++ tests/mod.rs
@@ -1,6 +1,15 @@
 use surrealdb::{engine::remote::ws::{Client, Ws}, opt::auth::Root, Surreal};
 use surrealdb_migrations::Migrations;
 
+
+/// Start the server with the following command:
+/// ```bash
+/// podman run -u root --rm -p 8000:8000 -v ./surrealdb/data:/surrealdb/data surrealdb/surrealdb:v1.1.1 start --auth --user root --pass root file:/surrealdb/data/mydatabase.db
+/// ```
+/// Connect to the server with the following command:
+/// ```bash
+///  podman run -it --rm --network=host surrealdb/surrealdb:v1.1.1 sql --endpoint ws://0.0.0.0:8000 -u root -p root
+/// ```
 #[tokio::test]
 async fn create_migration_table_if_not_exists() {
     tracing_subscriber::fmt::init();
